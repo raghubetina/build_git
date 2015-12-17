b538ca0a7f562fa2f68d373800800c2b9ac7fff0
Raghu Betina <rbetina@users.noreply.github.com>
2015-12-17 09:51:12 -0500

    VC can create snapshots of the state of the root folder.

--- vc.rb
+++ vc.rb
@@ -2,8 +2,14 @@ require "fileutils"
 require "digest/sha1"
 
 class VC
+  CONTENT_ROOT_FOLDER = "app"
+  BACKUPS_FOLDER = "vc_backups"
+  SNAPSHOTS_FOLDER = "vc_snapshots"
+
   def self.files
-    Dir.glob("app/**/*").select { |object| File.file?(object) }
+    Dir.glob("#{CONTENT_ROOT_FOLDER}/**/*").select do |object|
+      File.file?(object)
+    end
   end
 
   def self.hash(string)
@@ -14,26 +20,56 @@ class VC
     open(file).read
   end
 
-  def self.object_already_exists?(filename)
-    Dir.glob("vc_objects/*").include?(filename)
+  def self.object_already_exists?(folder, filename)
+    Dir.glob("#{folder}/*").include?(filename)
   end
 
-  def self.create_file_object(file)
-    destination = "vc_objects/#{hash(contents(file))}"
+  def self.create_version(file)
+    destination = "#{BACKUPS_FOLDER}/#{hash(contents(file))}"
 
-    unless object_already_exists?(destination)
-      puts "Backing up content."
+    unless object_already_exists?(BACKUPS_FOLDER, destination)
+      puts "Backing up #{file}"
       FileUtils.copy(file, destination)
     else
       puts "That content is already backed up."
     end
   end
 
-  def self.add
-    FileUtils.mkdir_p("vc_objects")
+  def self.backup
+    FileUtils.mkdir_p(BACKUPS_FOLDER)
 
-    files.each { |file| create_file_object(file) }
+    files.each { |file| create_version(file) }
+  end
+
+  def self.snapshot(message)
+    FileUtils.mkdir_p(SNAPSHOTS_FOLDER)
+
+    commit = ""
+
+    files.each do |file|
+      destination = "#{BACKUPS_FOLDER}/#{hash(contents(file))}"
+
+      if object_already_exists?(BACKUPS_FOLDER, destination)
+        commit << "#{destination}    #{file}\n"
+      else
+        puts "You have unsaved changes in #{file}. Please back them up before taking a snapshot."
+        return
+      end
+    end
+
+    destination = "#{SNAPSHOTS_FOLDER}/#{hash(commit)}"
+
+    unless object_already_exists?(SNAPSHOTS_FOLDER, destination)
+      puts "Creating snapshot."
+
+      commit << "\n#{message}\n"
+      commit << "#{Time.now}\n"
+
+      File.open(destination, "w") { |file| file.write(commit) }
+    else
+      puts "This snapshot has already been created."
+    end
   end
 end
 
-VC.send(ARGV[0])
+VC.send(*ARGV)
